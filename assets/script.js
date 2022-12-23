94bc7caf39b9bba3db7b2d23510064b1f6896742
rehanaf <rehanalfaqih287@gmail.com>
2022-12-23 13:09:12 +0700

    tinggal sayu hafizdh fira belum pp nyaa

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,33 @@ document.getElementById('semester').addEventListener('change', (e) => {
       break;
   }
 })
+
+const buttonLainnya = document.getElementById("lainnya");
+buttonLainnya.addEventListener("click", () => {
+  document.getElementById("hidden").classList.remove("hidden");
+  buttonLainnya.classList.add("hidden");
+});
+
+const nilaiValue = [978,977,975,970,969,967,965,965,960,958,955,954,952,952,952,951,951,948,947,946,946,945,943,941,941,937,933,933,930,927,921,906];
+
+
+const angka = document.querySelectorAll(".auto-number");
+const nilai = document.querySelectorAll(".nilai");
+for (let i = 0; i<32; i++) {
+  if (nilaiValue[i] == [nilaiValue[i-1]]) {
+    if (nilaiValue[i-1] == nilaiValue[i-2]) {
+      angka[i].innerHTML = i+2;
+    }
+    else {
+      angka[i].innerHTML = i+3;
+    }
+  }
+  else {
+    angka[i].innerHTML = i+4;
+  }
+  nilai[i].innerHTML = nilaiValue[i];
+};
+// Mecegah Kontroversi
+for (let i=0; i<nilai.length; i++) {
+  nilai[i].classList.add("opacity-0");
+};
